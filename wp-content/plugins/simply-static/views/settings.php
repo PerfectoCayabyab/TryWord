7f01d48043a477065245729499cece0b2b8f1bf0
Perfecto II Cayabyab <perfectocayabyab@gmail.com>
2023-04-23 01:07:06 +0800

    1st

---
+++ wp-content/plugins/simply-static/views/settings.php
@@ -0,0 +1,8 @@
+<div class="simply-static-admin-header">
+    <div class="logo"><img src="<br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function Simply_Static\esc_url() in C:\xampp\htdocs\try\wp-content\plugins\simply-static\views\settings.php:5
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\try\wp-content\plugins\simply-static\views\settings.php</b> on line <b>5</b><br />
+"></div>
+</div>
